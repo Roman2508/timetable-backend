df9981a678138930d76c3ec72c1f83539404ee22
Roman Ptashnyk <2508romaa@example.com>
2023-12-25 18:36:53 +0200

    schedule-lessons crud

--- src/schedule-lessons/schedule-lessons.module.ts
+++ src/schedule-lessons/schedule-lessons.module.ts
@@ -3,10 +3,11 @@ import { ScheduleLessonsService } from './schedule-lessons.service';
 import { ScheduleLessonsController } from './schedule-lessons.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScheduleLessonsEntity } from './entities/schedule-lesson.entity';
+import { StreamEntity } from 'src/streams/entities/stream.entity';
 
 @Module({
   controllers: [ScheduleLessonsController],
   providers: [ScheduleLessonsService],
-  imports: [TypeOrmModule.forFeature([ScheduleLessonsEntity])],
+  imports: [TypeOrmModule.forFeature([ScheduleLessonsEntity, StreamEntity])],
 })
 export class ScheduleLessonsModule {}
--- src/schedule-lessons/schedule-lessons.service.ts
+++ src/schedule-lessons/schedule-lessons.service.ts
@@ -1,15 +1,24 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { CreateScheduleLessonDto } from './dto/create-schedule-lesson.dto';
-import { UpdateScheduleLessonDto } from './dto/update-schedule-lesson.dto';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { ScheduleLessonsEntity } from './entities/schedule-lesson.entity';
+import { CreateScheduleLessonDto } from './dto/create-schedule-lesson.dto';
+import { UpdateScheduleLessonDto } from './dto/update-schedule-lesson.dto';
+import { StreamEntity } from 'src/streams/entities/stream.entity';
 
 @Injectable()
 export class ScheduleLessonsService {
   constructor(
     @InjectRepository(ScheduleLessonsEntity)
     private repository: Repository<ScheduleLessonsEntity>,
+
+    @InjectRepository(StreamEntity)
+    private streamRepository: Repository<StreamEntity>,
   ) {}
 
   async create(dto: CreateScheduleLessonDto) {
@@ -27,6 +36,40 @@ export class ScheduleLessonsService {
 
     if (lessonsOverlay) {
       throw new BadRequestException('Можливі накладки занять');
+    }
+
+    // Перевірити чи є потоки, якщо є - виставити для всіх груп в потоці
+    if (dto.stream) {
+      const stream = await this.streamRepository.findOne({
+        where: { id: dto.stream },
+        relations: { groups: true },
+        select: { groups: { id: true } },
+      });
+
+      if (!stream) {
+        throw new NotFoundException(
+          'Помилка при створенні елемента розкладу для потоку',
+        );
+      }
+
+      const newLessons = await Promise.all(
+        stream.groups.map(async (el) => {
+          const { group, teacher, auditory, stream, ...rest } = dto;
+
+          const newLesson = this.repository.create({
+            ...rest,
+            group: { id: el.id },
+            teacher: { id: teacher },
+            auditory: { id: auditory },
+            stream: { id: stream },
+          });
+
+          return this.repository.save(newLesson);
+        }),
+      );
+
+      // Можливо el.group.id === undefined !!!!!!!!!!!!!!!!!!!!!
+      return newLessons.find((el) => el.group.id === dto.group);
     }
 
     const { group, teacher, auditory, stream, ...rest } = dto;
@@ -58,8 +101,9 @@ export class ScheduleLessonsService {
   //   return `This action returns a #${id} scheduleLesson`;
   // }
 
+  // Оновити аудиторію для виставленого елемента розкладу
   async update(id: number, dto: UpdateScheduleLessonDto) {
-    const lessons = await this.repository.findOne({
+    const lesson = await this.repository.findOne({
       where: { id },
       relations: {
         group: true,
@@ -67,8 +111,42 @@ export class ScheduleLessonsService {
         stream: true,
       },
     });
+
     // Якщо група об`єднана в потік потрібно оновити аудиторію для всіх груп потоку
-    const { group, teacher, auditory, stream, ...rest } = lessons;
+
+    if (!lesson) throw new NotFoundException('Не знайдено');
+
+    // Якщо група об'єднана в потік і кількість груп в потоці більше 1
+    if (lesson.stream && lesson.stream.groups.length > 1) {
+      const currentsLessonDate = {
+        date: lesson.date,
+        lessonNumber: lesson.lessonNumber,
+      };
+
+      const streamLessonsInCurrentDate = await this.repository.find({
+        where: {
+          name: lesson.name,
+          semester: lesson.semester,
+          date: currentsLessonDate.date,
+          stream: { id: lesson.stream.id },
+          lessonNumber: currentsLessonDate.lessonNumber,
+        },
+      });
+
+      Promise.all(
+        streamLessonsInCurrentDate.map(async (el) => {
+          return this.repository.save({
+            ...el,
+            auditory: { id: dto.auditory },
+          });
+        }),
+      );
+
+      return streamLessonsInCurrentDate.find((el) => el.id === id)[0];
+    }
+
+    // Якщо група не об'єднана в потік
+    const { group, teacher, auditory, stream, ...rest } = lesson;
 
     return this.repository.save({
       ...rest,
@@ -76,7 +154,56 @@ export class ScheduleLessonsService {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} scheduleLesson`;
+  async remove(id: number) {
+    const lesson = await this.repository.findOne({
+      where: { id },
+      relations: {
+        stream: true,
+      },
+    });
+
+    if (!lesson) throw new NotFoundException('Не знайдено');
+
+    // Якщо група об'єднана в потік і кількість груп в потоці більше 1
+    if (lesson.stream && lesson.stream.groups.length > 1) {
+      const currentsLessonDate = {
+        date: lesson.date,
+        lessonNumber: lesson.lessonNumber,
+      };
+
+      const streamLessonsInCurrentDate = await this.repository.find({
+        where: {
+          name: lesson.name,
+          semester: lesson.semester,
+          date: currentsLessonDate.date,
+          stream: { id: lesson.stream.id },
+          lessonNumber: currentsLessonDate.lessonNumber,
+        },
+      });
+
+      // Потрібно перевіряти чи були видалені елементи, якщо ні - повернути помилку !!!!!
+
+      Promise.all(
+        streamLessonsInCurrentDate.map(async (el) => {
+          const res = await this.repository.delete({ id: el.id });
+
+          if (res.affected === 0) {
+            throw new NotFoundException('Не знайдено');
+          }
+        }),
+      );
+
+      return id;
+      // return streamLessonsInCurrentDate.find((el) => el.id === id)[0];
+    }
+
+    // Якщо дисипліна не об'єднана в потік
+    const res = await this.repository.delete({ id });
+
+    if (res.affected === 0) {
+      throw new NotFoundException('Не знайдено');
+    }
+
+    return id;
   }
 }
