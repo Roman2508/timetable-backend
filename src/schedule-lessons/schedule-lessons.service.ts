a3cc9d0c0e91a2af373e14d8998b069048941f11
Roman2508 <2508romaa@gmail.com>
2024-04-16 16:18:28 +0300

    calendar events

--- src/schedule-lessons/schedule-lessons.service.ts
+++ src/schedule-lessons/schedule-lessons.service.ts
@@ -1,17 +1,33 @@
+const path = require('path');
+const fs = require('fs').promises;
+
 import {
   Injectable,
   NotFoundException,
   BadRequestException,
 } from '@nestjs/common';
 import * as dayjs from 'dayjs';
+import { google } from 'googleapis';
 import { Between, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { authenticate } from '@google-cloud/local-auth';
 
 import { StreamEntity } from 'src/streams/entities/stream.entity';
 import { SettingsEntity } from 'src/settings/entities/setting.entity';
 import { ScheduleLessonsEntity } from './entities/schedule-lesson.entity';
 import { CreateScheduleLessonDto } from './dto/create-schedule-lesson.dto';
 import { UpdateScheduleLessonDto } from './dto/update-schedule-lesson.dto';
+
+const TOKEN_PATH = path.join(process.cwd(), 'src/reserved-lessons/token.json');
+const CREDENTIALS_PATH = path.join(
+  process.cwd(),
+  'src/reserved-lessons/client_secret.json',
+);
+const SCOPES = [
+  'https://www.googleapis.com/auth/calendar',
+  'https://www.googleapis.com/auth/meetings',
+  'https://www.googleapis.com/auth/meetings.space.created',
+];
 
 @Injectable()
 export class ScheduleLessonsService {
@@ -25,6 +41,106 @@ export class ScheduleLessonsService {
     @InjectRepository(SettingsEntity)
     private settingsRepository: Repository<SettingsEntity>,
   ) {}
+
+  async loadSavedCredentialsIfExist() {
+    try {
+      const content = await fs.readFile(TOKEN_PATH);
+      const string = content.toString();
+      const credentials = JSON.parse(string);
+      return google.auth.fromJSON(credentials);
+    } catch (err) {
+      // console.error('Помилка завантаження збережених облікових даних:', err);
+      return null;
+    }
+  }
+
+  async saveCredentials(client) {
+    const content = await fs.readFile(CREDENTIALS_PATH);
+    const keys = JSON.parse(content);
+    const key = keys.installed || keys.web;
+    const payload = JSON.stringify({
+      type: 'authorized_user',
+      client_id: key.client_id,
+      client_secret: key.client_secret,
+      refresh_token: client.credentials.refresh_token,
+    });
+    await fs.writeFile(TOKEN_PATH, payload);
+  }
+
+  async authorize() {
+    let client: any = await this.loadSavedCredentialsIfExist();
+
+    if (client) {
+      return client;
+    }
+
+    client = await authenticate({
+      scopes: SCOPES,
+      keyfilePath: CREDENTIALS_PATH,
+    });
+
+    if (client.credentials) {
+      await this.saveCredentials(client);
+    }
+    return client;
+  }
+
+  async createCalendarEvent(dto: any) {
+    const auth = await this.authorize();
+    const calendar = google.calendar({ version: 'v3', auth });
+    const event = {
+      summary: 'Google I/O 2024',
+      location: '800 Howard St., San Francisco, CA 94103',
+      description: "A chance to hear more about Google's developer products.",
+      start: {
+        dateTime: '2024-02-25T09:00:00-07:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      conferenceData: {
+        createWithGoogleMeet: true,
+      },
+      end: {
+        dateTime: '2024-02-25T17:00:00-08:00',
+        timeZone: 'America/Los_Angeles',
+      },
+      recurrence: ['RRULE:FREQ=DAILY;COUNT=2'],
+      attendees: [
+        { email: 'roma.250899@gmail.com' },
+        // { email: 'sbrin@example.com' },
+      ],
+      reminders: {
+        useDefault: false,
+        overrides: [
+          { method: 'email', minutes: 24 * 60 },
+          { method: 'popup', minutes: 10 },
+        ],
+      },
+    };
+    const responce = await calendar.events.insert({
+      auth: auth,
+      calendarId: 'primary',
+      conferenceDataVersion: 1,
+      requestBody: {
+        start: { dateTime: '2024-02-26T10:00:00', timeZone: 'Europe/Kyiv' },
+        end: { dateTime: '2024-02-26T11:00:00', timeZone: 'Europe/Kyiv' },
+        conferenceData: {
+          conferenceId: 'hangoutsMeet',
+          createRequest: {
+            conferenceSolutionKey: {
+              type: 'hangoutsMeet',
+            },
+          },
+        },
+      },
+    });
+    // Отримання ідентифікатора конференції Google Meet
+    const meetLink = responce.data.conferenceData.entryPoints[0].uri;
+    console.log('Conference URL:', meetLink);
+    console.log(responce.data.conferenceData);
+    // @ts-ignore
+    console.log(responce.config.data.conferenceData);
+    return true;
+  }
 
   async findOneByDateAndGroup(
     date: Date,
